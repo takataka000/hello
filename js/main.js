2029a0ad837cddfaebfc0f64b6a4e9229cf2caac
komatsuzaki <68366074+takataka000@users.noreply.github.com>
2020-07-18 22:44:55 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1618,7 +1618,7 @@ var myChart7 = new Chart(ctx, {
     datasets: [
       {
         label: "楽天",
-        data: [9.7,	11.5,	9.3,	11.5,	16.2,	13.3,	13.9, 276000],
+        data: [9.7,	11.5,	9.3,	11.5,	16.2,	13.3,	13.9, 2760000],
         backgroundColor: [
           "#A7D895",
           "#A7D895",
@@ -1735,8 +1735,8 @@ var myChart7 = new Chart(ctx, {
             beginAtZero: true,
             // fontColor: '#000',
             fontSize: 10,
-            min: 10000,
-            max: 1000000,
+            min: 100000,
+            max: 3000000,
           },
           gridLines: {
             color: 'rgba(0, 0, 0, 0.05)',
