47a75bd8bb84347704f520b9ee17acdf9fdb88dc
komatsuzaki <68366074+takataka000@users.noreply.github.com>
2020-07-21 02:38:42 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1387,7 +1387,7 @@ var myChart10 = new Chart(ctx, {
         boxWidth: 10,
         fontSize: 10,
         fontColor: '#000',
-        padding: 6
+        padding: 5.5
       },
     },
     scales: {
