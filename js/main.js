bbd8f4ceab6763e4678edbdac5952c75cb75dd99
komatsuzaki <68366074+takataka000@users.noreply.github.com>
2020-07-21 02:08:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1233,16 +1233,8 @@ var myChart9 = new Chart(ctx, {
           return data.labels[tooltipItem.index]
             + ": "
             + data.datasets[0].data[tooltipItem.index]
-            + " %";
+            + " 人";
         },
-      },
-    },
-    layout: {
-      padding: {
-        top: 20,
-        left: 10,
-        bottom: 0,
-        right: 10
       },
     },
     elements: {
@@ -1253,8 +1245,15 @@ var myChart9 = new Chart(ctx, {
         // "fill": "-1"
       },
   },
+    layout: {
+      padding: {
+        top: 20,
+        left: 10,
+        bottom: 0,
+        right: 30
+      },
+    },
     legend: {
-      display: false,
       position: 'bottom',
       fullWidth: true,
       labels: {
@@ -1269,8 +1268,8 @@ var myChart9 = new Chart(ctx, {
         {
           ticks: {
             beginAtZero: true,
-            fontColor: '#000',
             fontSize: 10,
+            fontColor: '#000',
             min: 0,
             max: 50,
           },
@@ -1278,6 +1277,7 @@ var myChart9 = new Chart(ctx, {
             color: 'rgba(0, 0, 0, 0.05)',
             borderDash: [6, 4],
           },
+          stacked: false,
         },
       ],
       xAxes: [
@@ -1294,8 +1294,83 @@ var myChart9 = new Chart(ctx, {
         },
       ],
     },
+    "plugins": {
+      "filler": {
+        "propagate": true
+      },
+    },
     animation: {
       duration: 2000
     },
+    // tooltips: {
+    //   callbacks: {
+    //     label: function (tooltipItem, data) {
+    //       return data.labels[tooltipItem.index]
+    //         + ": "
+    //         + data.datasets[0].data[tooltipItem.index]
+    //         + " %";
+    //     },
+    //   },
+    // },
+    // layout: {
+    //   padding: {
+    //     top: 20,
+    //     left: 10,
+    //     bottom: 0,
+    //     right: 10
+    //   },
+    // },
+    // elements: {
+    //   line: {
+    //       tension: 0, // ベジェ曲線を無効にする
+    //       fill: false,
+    //       "stack": 1,
+    //     // "fill": "-1"
+    //   },
+    // },
+    // legend: {
+    //   display: false,
+    //   position: 'bottom',
+    //   fullWidth: true,
+    //   labels: {
+    //     boxWidth: 10,
+    //     fontSize: 10,
+    //     fontColor: '#000',
+    //     padding: 10
+    //   },
+    // },
+    // scales: {
+    //   yAxes: [
+    //     {
+    //       ticks: {
+    //         beginAtZero: true,
+    //         fontColor: '#000',
+    //         fontSize: 10,
+    //         min: 0,
+    //         max: 50,
+    //       },
+    //       gridLines: {
+    //         color: 'rgba(0, 0, 0, 0.05)',
+    //         borderDash: [6, 4],
+    //       },
+    //     },
+    //   ],
+    //   xAxes: [
+    //     {
+    //       ticks: {
+    //         beginAtZero: true,
+    //         fontColor: '#000',
+    //         fontSize: 10,
+    //       },
+    //       gridLines: {
+    //         display: false,
+    //         color: 'rgba(0, 0, 0, 0.05)'
+    //       },
+    //     },
+    //   ],
+    // },
+    // animation: {
+    //   duration: 2000
+    // },
   },
 });
